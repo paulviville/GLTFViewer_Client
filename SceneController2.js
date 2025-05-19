6322f82a7257dbc62cc8ca7595c1899df70c617e
paulviville <paul.viville@gmail.com>
2025-05-19 19:20:04 +0200

    selection

--- SceneController2.js
+++ SceneController2.js
@@ -15,22 +15,38 @@ export default class SceneController {
 
 	#gui = new GUI();
 	#guiParams = {
+		previousLabel: undefined,
         previouslySelected: undefined,
         selected: "none",
     }
 
+	#selectedNode = null;
 	#transformDummy = new THREE.Object3D();
 	#transformControls;
 	#target;
 
-    #camera;
+
 	#renderer;
 
+    #camera;
 	#orbitControls;
 	#cameraNeedsUpdate = false;
 
+	#onMouseDownBound;
+	#onMouseMoveBound;
+	#onMouseUpBound;
+
 	#mouse = new THREE.Vector2();
-	#raycaster;
+	#lastPointerMouse = new THREE.Vector2();
+	#raycaster = new THREE.Raycaster();
+
+	#pointer = {
+		p0: new THREE.Vector3(),
+		p1: new THREE.Vector3()
+	};
+	#pointerNeedsUpdate = false; 
+	#pointerActive = false; 
+
 
 	constructor ( sceneInterface, sceneDescriptor ) {
 		console.log(`SceneController - constructor`);
@@ -63,6 +79,7 @@ export default class SceneController {
 
 
 		this.#initializeGui();
+		this.#initializeMouseControls();
 	}
 
 	set clientManager ( clientManager ) {
@@ -78,8 +95,13 @@ export default class SceneController {
             "selected",
             ["none", ...this.#sceneInterface.objectsMap.keys()]
         ).onChange( label => {
-			if(label === "none")
+			if(this.#selectedNode !== null) {
+				this.#requestDeselectNode(this.#selectedNode);
+			}
+
+			if(label === "none") {
 				return;
+			}
 
 			this.#requestSelectNode(label);
         });
@@ -117,7 +139,8 @@ export default class SceneController {
 			console.log("selected by you");
         	this.#sceneDescriptor.selectNode(node);
 
-			this.#setTransformTarget(nodeId);
+			this.#selectedNode = nodeId;
+			this.#setTransformTarget();
 			this.#onTransformStart();
 		}
 		else {
@@ -134,7 +157,9 @@ export default class SceneController {
 		const node = this.#sceneDescriptor.getNode(nodeId);
         this.#sceneDescriptor.deselectNode(node);
 
-		if ( userId == this.#clientManager.userId ) { 
+		if ( nodeId == this.#selectedNode ) { 
+		// if ( userId == this.#clientManager.userId ) {
+			this.#selectedNode = null;
 			this.#onTransformEnd();
 		}
 
@@ -153,7 +178,9 @@ export default class SceneController {
 		// this.#transformControls.setMode(mode);
 	}
 
-	#setTransformTarget ( nodeId ) {
+	#setTransformTarget ( ) {
+		/// replace for multiselection
+		const nodeId = this.#selectedNode;
 		const node = this.#sceneDescriptor.getNode(nodeId);
 		
         const matrix = this.#sceneDescriptor.getMatrix(node);
@@ -193,25 +220,66 @@ export default class SceneController {
 	}
 
 	#onTransformEnd ( ) {
-		console.log(`SceneController - onTransformEnd`);
+		console.log(`SceneController - #onTransformEnd`);
 
 		this.#sceneInterface.scene.remove(this.#transformControls.getHelper());
 		this.#transformControls.enabled = false;
 	}
 
-    #setMouse ( x, y ) { 
+	#initializeMouseControls ( ) {
+		console.log(`SceneController - #initializeMouseControls`);
 
+		this.#onMouseDownBound = this.#onMouseDown.bind(this);
+        this.#onMouseMoveBound = this.#onMouseMove.bind(this);
+        this.#onMouseUpBound = this.#onMouseUp.bind(this);
+
+		this.#renderer.domElement.addEventListener("mousedown", this.#onMouseDownBound);
+
+	}
+
+    #setMouse ( x, y ) {
+		// console.log(`SceneController - #setMouse`);
+
+        this.#mouse.set(
+			(x / window.innerWidth) * 2 - 1,
+			- (y / window.innerHeight) * 2 + 1
+		);
 	}
 
 	#onMouseDown ( event ) {
+		console.log(`SceneController - #onMouseDown`);
+        this.#setMouse(event.clientX, event.clientY);
 
+		if( event.button == 1 ) {
+			this.#onPointerStart();
+        	this.#pointerNeedsUpdate = true;
+        	this.#pointerActive = true;
+			this.#lastPointerMouse.copy(this.#mouse);
+            this.#renderer.domElement.addEventListener("mouseup", this.#onMouseUpBound);
+		}
 	}
 
     #onMouseMove ( event ) {
+		// console.log(`SceneController - #onMouseMove`);
 
+        this.#setMouse(event.clientX, event.clientY);
+        this.#pointerNeedsUpdate = this.#pointerActive;
+
+		if( this.#pointerActive ) {
+			this.#lastPointerMouse.copy(this.#mouse);
+
+		}
 	}
 
 	#onMouseUp ( event ) {
+		console.log(`SceneController - #onMouseUp`);
+
+		if( this.#pointerActive ) {
+			this.#onPointerEnd();
+		}
+        this.#pointerActive = false;
+		this.#lastPointerMouse.copy(this.#mouse);
+        this.#renderer.domElement.removeEventListener("mouseup", this.#onMouseUpBound);
 
 	}
 
